c93ad1f124fbe2b2a51487e21e162b5ebcf38ec8
ArchLeaders <archparliament@gmail.com>
2022-02-23 16:10:32 -0800

    Installer User Interface

---
+++ BotwInstaller.Wizard/ViewModels/ShellViewModel.cs
@@ -0,0 +1,75 @@
+﻿#pragma warning disable CS0108
+#pragma warning disable CS8612
+#pragma warning disable CS8618
+
+using Stylet;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace BotwInstaller.Wizard.ViewModels
+{
+    public class ShellViewModel : Screen, INotifyPropertyChanged
+    {
+        #region Actions
+
+        public async Task Install()
+        {
+            ShowDialog("Warning!", "MARIO SUCKS!");
+        }
+
+        public void ShowDialog(string message, string? exMessage = null, string title = "Notice", bool isYesNo = false, string? exColor = null)
+        {
+            PromptViewModel promptViewModel = new(message, title, isYesNo, exMessage, exColor);
+            windowManager.ShowDialog(promptViewModel);
+        }
+
+        #endregion
+
+        #region Props
+
+        private string _controllerApi = "Controller -";
+        public Dictionary<string, string> ControllerApiTranslate { get; } = new()
+        {
+            { "Controller -", "XInput\n\n\n\n\n\n\n\n\n\n\nTEXT" },
+            { "XBox | XBox Emulated DS4", "XInput" },
+            { "Nintendo Switch Joycons", "SDLController-Joycon" },
+            { "Nintendo Switch Pro Controller", "SDLController" },
+            { "Emulated DS4", "DSUController" },
+            { "Keyboard (Not Recomended)", "Keyboard" },
+        };
+        public string ControllerApi
+        {
+            get { return _controllerApi; }
+            set
+            {
+                _controllerApi = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        #endregion
+
+        #region Bindings
+
+        private Visibility page = Visibility.Hidden;
+
+        #endregion
+
+        private IWindowManager windowManager;
+        public ShellViewModel(IWindowManager windowManager)
+        {
+            this.windowManager = windowManager;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ BotwInstaller.Wizard/Views/ShellView.xaml.cs
@@ -0,0 +1,142 @@
+﻿#pragma warning disable CS8600
+#pragma warning disable CS8604
+#pragma warning disable CS8605
+#pragma warning disable CS8629
+
+using BotwInstaller.Wizard.ViewModels;
+using BotwInstaller.Wizard.ViewThemes.App;
+using BotwScripts.Lib.Common.Computer;
+using Stylet;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Xml;
+
+namespace BotwInstaller.Wizard.Views
+{
+    /// <summary>
+    /// Interaction logic for ShellView.xaml
+    /// </summary>
+    public partial class ShellView : Window
+    {
+        private IWindowManager windowManager;
+        public ShellView()
+        {
+            #region Template Setters
+
+            InitializeComponent();
+            DataContext = new ShellViewModel(windowManager);
+
+            if (File.Exists(ShellViewTheme.ThemeFile))
+            {
+                ShellViewTheme.ThemeStr = "Light";
+                Animation.ThicknessAnim(footerChangeAppTheme_GridParent, nameof(footerChangeAppTheme_IsLight), Grid.MarginProperty, new Thickness(0), 100);
+                ShellViewTheme.Change(true);
+            }
+            else
+            {
+                ShellViewTheme.ThemeStr = "Dark";
+                Animation.ThicknessAnim(footerChangeAppTheme_GridParent, nameof(footerChangeAppTheme_IsDark), Grid.MarginProperty, new Thickness(0), 100);
+                ShellViewTheme.Change();
+            }
+
+            // Load button close/minimize events
+            btnExit.Click += (s, e) => { Hide(); Environment.Exit(1); };
+            btnMin.Click += (s, e) => WindowState = WindowState.Minimized;
+            btnReSize.Click += (s, e) =>
+            {
+
+                if (WindowState == WindowState.Normal)
+                    WindowState = WindowState.Maximized;
+                else
+                    WindowState = WindowState.Normal;
+            };
+
+            // Load window fix
+            SourceInitialized += (s, e) =>
+            {
+
+            };
+
+            // Assign state changed events
+            shellView.StateChanged += (s, e) =>
+            {
+
+                if (WindowState == WindowState.Normal)
+                {
+                    rectCascade.Opacity = 0;
+                    rectMaximize.Opacity = 1;
+                }
+                else
+                {
+                    rectCascade.Opacity = 1;
+                    rectMaximize.Opacity = 0;
+                }
+            };
+
+            // Change app theme event
+            footerChangeAppTheme.Click += async (s, e) =>
+            {
+                if (ShellViewTheme.ThemeStr == "Dark")
+                {
+                    ShellViewTheme.ThemeStr = "Light";
+                    ShellViewTheme.Change(true);
+                    Animation.ThicknessAnim(footerChangeAppTheme_GridParent, nameof(footerChangeAppTheme_IsDark), Grid.MarginProperty, new Thickness(0, 0, 0, 34), 250);
+                    await Task.Run(() => Thread.Sleep(200));
+                    Animation.ThicknessAnim(footerChangeAppTheme_GridParent, nameof(footerChangeAppTheme_IsLight), Grid.MarginProperty, new Thickness(0), 200);
+                }
+                else
+                {
+                    ShellViewTheme.ThemeStr = "Dark";
+                    ShellViewTheme.Change();
+                    Animation.ThicknessAnim(footerChangeAppTheme_GridParent, nameof(footerChangeAppTheme_IsLight), Grid.MarginProperty, new Thickness(0, 34, 0, 0), 250);
+                    await Task.Run(() => Thread.Sleep(200));
+                    Animation.ThicknessAnim(footerChangeAppTheme_GridParent, nameof(footerChangeAppTheme_IsDark), Grid.MarginProperty, new Thickness(0), 200);
+                }
+            };
+
+            footerRequestHelp.Click += async (s, e) => await HiddenProcess.Start("explorer.exe", "https://github.com/ArchLeaders/BotwInstaller-RE#readme");
+
+            #endregion
+
+            // . . .
+        }
+
+        private void Hyperlink_RequestNavigate(object sender, System.Windows.Navigation.RequestNavigateEventArgs e)
+        {
+            Hyperlink link = (Hyperlink)sender;
+            Process.Start("explorer.exe", link.NavigateUri.ToString());
+        }
+
+        private void InstallWiiU_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void InstallCemu_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void InstallSwitch_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
