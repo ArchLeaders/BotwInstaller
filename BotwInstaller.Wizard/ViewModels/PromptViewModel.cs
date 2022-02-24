b5bcd54bfa6f47c0cf8d11c475b4a4c72d5afe0a
ArchLeaders <archparliament@gmail.com>
2022-02-23 16:11:22 -0800

    Promt View and ViewModel

---
+++ BotwInstaller.Wizard/ViewModels/PromptViewModel.cs
@@ -0,0 +1,193 @@
+﻿#pragma warning disable CS0108
+#pragma warning disable CS8600
+#pragma warning disable CS8601
+#pragma warning disable CS8612
+#pragma warning disable CS8618
+
+using MaterialDesignThemes.Wpf;
+using Stylet;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Media;
+
+namespace BotwInstaller.Wizard.ViewModels
+{
+    public class PromptViewModel : Screen, INotifyPropertyChanged
+    {
+        #region Actions
+
+        public void Yes()
+        {
+            RequestClose(false);
+        }
+
+        public void No()
+        {
+            RequestClose(true);
+        }
+
+        #endregion
+
+        #region Props
+
+        private bool _isYesNo = false;
+        public bool IsYesNo
+        {
+            get { return _isYesNo; }
+            set
+            {
+                _isYesNo = value;
+
+                if (_isYesNo)
+                {
+                    OkMode = "No";
+                    YesBtnVisibility = Visibility.Visible;
+                }
+                else
+                {
+                    OkMode = "Ok";
+                    YesBtnVisibility = Visibility.Collapsed;
+                }
+
+                NotifyPropertyChanged();
+            }
+        }
+
+
+        private string _title = "Notice";
+        public string Title
+        {
+            get { return _title; }
+            set
+            {
+                _title = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private string _extendedMessage = "";
+        public string ExtendedMessage
+        {
+            get { return _extendedMessage; }
+            set
+            {
+                _extendedMessage = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private string _message = "No details were provided.";
+        public string Message
+        {
+            get { return _message; }
+            set
+            {
+                _message = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        #endregion
+
+        #region Bindings
+
+        private static ITheme GetTheme()
+        {
+            PaletteHelper helper = new();
+            return helper.GetTheme();
+        }
+
+        private Brush _foreground = new SolidColorBrush(GetTheme().Body);
+        public Brush Foreground
+        {
+            get { return _foreground; }
+            set
+            {
+                _foreground = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private double _detailHeight = 10.0;
+        public double DetailHeight
+        {
+            get { return _detailHeight; }
+            set
+            {
+                _detailHeight = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private double _width = 220.0;
+        public double Width
+        {
+            get { return _width; }
+            set
+            {
+                _width = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private string _okMode = "Ok";
+        public string OkMode
+        {
+            get { return _okMode; }
+            set
+            {
+                _okMode = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private Visibility _btnVisibility = Visibility.Hidden;
+        public Visibility YesBtnVisibility
+        {
+            get { return _btnVisibility; }
+            set
+            {
+                _btnVisibility = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private Visibility _detailVisibility = Visibility.Hidden;
+        public Visibility DetailVisibility
+        {
+            get { return _detailVisibility; }
+            set
+            {
+                _detailVisibility = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        #endregion
+
+        public PromptViewModel(string message, string title = "Notice", bool isYesNo = false, string? extendedMessage = null, string? extendedMessageColor = null)
+        {
+            Message = message;
+            Title = title;
+            IsYesNo = isYesNo;
+
+            if (extendedMessage != null)
+            {
+                DetailVisibility = Visibility.Visible;
+                ExtendedMessage = extendedMessage;
+                DetailHeight = 160;
+                Width = 300;
+
+                if (extendedMessageColor != null)
+                    Foreground = (Brush)new BrushConverter().ConvertFromString(extendedMessageColor);
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ BotwInstaller.Wizard/Views/PromptView.xaml.cs
@@ -0,0 +1,30 @@
+﻿using BotwInstaller.Wizard.ViewModels;
+using System;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace BotwInstaller.Wizard.Views
+{
+    /// <summary>
+    /// Interaction logic for PromptView.xaml
+    /// </summary>
+    public partial class PromptView : Window
+    {
+        public PromptView()
+        {
+            InitializeComponent();
+            DataContext = new PromptViewModel("No details were provided.");
+            SourceInitialized += (s, a) =>
+            {
+                Dispatcher.Invoke(InvalidateVisual, DispatcherPriority.Input);
+            };
+
+            btnOk.Focus();
+        }
+
+        private void Window_OnContentRendered(object sender, EventArgs e)
+        {
+            InvalidateVisual();
+        }
+    }
+}
