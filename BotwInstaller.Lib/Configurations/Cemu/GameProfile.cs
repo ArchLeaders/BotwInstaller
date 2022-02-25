3a9ff4dd8af2e76e6dc1653a2a033a4ba5f92ade
ArchLeaders <archparliament@gmail.com>
2022-02-24 18:06:08 -0800

    Cemu game profile, writer and objects

---
+++ BotwInstaller.Lib/Configurations/Cemu/GameProfile.cs
@@ -0,0 +1,50 @@
+﻿using BotwScripts.Lib.Common.ClassObjects.Ini;
+
+namespace BotwInstaller.Lib.Configurations.Cemu
+{
+    public class GameProfile
+    {
+        public static void Write(Config conf)
+        {
+            Directory.CreateDirectory($"{conf.Dirs.Cemu}\\gameProfiles");
+            IniFile ini = new($"{conf.Dirs.Cemu}\\gameProfiles\\{GameInfo.GetTitleID(conf.Dirs.Base)}.ini");
+
+            Api = conf.ControllerApi;
+            GameProfile gameProfile = new();
+
+            foreach (var category in gameProfile.Profile)
+                foreach (var entry in category.Value)
+                    ini.Write(entry.Key, entry.Value, category.Key);
+        }
+
+        private static string Api { get; set; } = "XInput";
+
+        public Dictionary<string, Dictionary<string, string>> Profile = new()
+        {
+            {
+                "General", new() {
+                    { "loadSharedLibraries", "true" },
+                    { "startWithPadView", "false" }
+                }
+            },
+            {
+                "CPU", new() {
+                    { "cpuMode", "Multi-core recompiler" },
+                    { "threadQuantum", "45000" }
+                }
+            },
+            {
+                "Graphics", new() {
+                    { "accurateShaderMul", "true" },
+                    { "precompileShaders", "auto" },
+                    { "graphics_api", "1" }
+                }
+            },
+            {
+                "Controller", new() {
+                    { "controller1", $"{Api}.txt" }
+                }
+            },
+        };
+    }
+}
