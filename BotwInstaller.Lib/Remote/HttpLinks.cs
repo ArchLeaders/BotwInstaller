0e757468435dacb4e64b8879baee8c6b11bbf258
ArchLeaders <archparliament@gmail.com>
2022-02-24 20:49:53 -0800

    Move HttpLink

---
+++ BotwInstaller.Lib/Remote/HttpLinks.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BotwInstaller.Lib.Remote
+{
+    public class HttpLinks
+    {
+        public static string ModPresetsJson { get; } = "https://raw.githubusercontent.com/ArchLeaders/Botw-Installer/master/RE/TempDataFromRE.json";
+        public static string BotwBatchFile { get; } = "";
+        public static string BcmlBatchFile { get; } = "";
+        public static string CemuBatchFile { get; } = "";
+        public static string DS4BatchFile { get; } = "";
+
+    }
+}
