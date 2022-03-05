6513115ffb3b7a0f849158cbb245b7e433a18c8e
ArchLeaders <archparliament@gmail.com>
2022-03-05 14:42:01 -0800

    Run BOTW batch script

--- BotwInstaller.Lib/Configurations/BatchFiles/botw.bat
+++ BotwInstaller.Lib/Configurations/BatchFiles/botw.bat
@@ -1,7 +1,7 @@
 ﻿@ECHO OFF
 
 :: Start BotW
-START "BOTW" "$cemu\Cemu.exe" -g -f "$botw\code\U-King.rpx"
+START "BOTW" "$cemu\Cemu.exe" -g -f "$base\code\U-King.rpx"
 
 :: Start DS4Windows if it exists
 IF EXIST "$ds4\DS4Windows.exe" (
