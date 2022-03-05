6468182d9370ebaacbed4a7ecd33c1b0813947fb
ArchLeaders <archparliament@gmail.com>
2022-03-05 14:44:22 -0800

    BCML installer; Mod installer (WIP)

--- BotwInstaller.Lib/Installer.cs
+++ BotwInstaller.Lib/Installer.cs
@@ -1,4 +1,5 @@
-﻿using BotwScripts.Lib.Common;
+﻿using BotwInstaller.Lib.Configurations.Shortcuts;
+using BotwScripts.Lib.Common;
 using BotwScripts.Lib.Common.Computer.Software;
 using BotwScripts.Lib.Common.Computer.Software.Resources;
 using BotwScripts.Lib.Common.IO;
@@ -13,41 +14,118 @@ namespace BotwInstaller.Lib
 {
     public class Installer
     {
-        public static async Task RunInstallerAsync(Interface.Notify print, Interface.Update update, Interface.Error error, Config c)
+        public static async Task<Config> RunInstallerAsync(Interface.Notify print, Interface.Update update, Interface.Error error, Config conf, bool nx = false)
         {
-            Config conf = new();
+            if (nx)
+            {
 
-            /// Get system information and update config -\-
+            }
+            else
+            {
+                // Get system information
+                Dictionary<string, object> gameInfo = await GameInfo.GetFiles(print, conf.Dirs.Dynamic, conf.Dirs.Python);
 
-            Dictionary<string, object> gameInfo = await GameInfo.GetFiles(conf.Dirs.Cemu, conf.Dirs.Python);
+                // Set dictionary values to static parameters
+                conf.Dirs.Dynamic = (string)gameInfo["Cemu"] == "NOT FOUND" ? conf.Dirs.Dynamic : (string)gameInfo["Cemu"];
+                conf.Dirs.Python = (string)gameInfo["Python"] == "NOT FOUND" ? conf.Dirs.Python : (string)gameInfo["Python"];
 
-            conf.Dirs.Cemu = (string)gameInfo["Cemu"] == "NOT FOUND" ? conf.Dirs.Cemu : (string)gameInfo["Cemu"];
-            conf.Dirs.Python = (string)gameInfo["Python"] == "NOT FOUND" ? conf.Dirs.Python : (string)gameInfo["Python"];
+                conf.Dirs.Base = (string)gameInfo["Game"] == "NOT FOUND" ? "" : (string)gameInfo["Game"];
+                conf.Dirs.Update = (string)gameInfo["Update"] == "NOT FOUND" ? "" : (string)gameInfo["Update"];
+                conf.Dirs.DLC = (string)gameInfo["DLC"] == "NOT FOUND" ? "" : (string)gameInfo["DLC"];
 
-            conf.Dirs.Base = (string)gameInfo["Game"] == "NOT FOUND" ? "" : (string)gameInfo["Game"];
-            conf.Dirs.Update = (string)gameInfo["Update"] == "NOT FOUND" ? "" : (string)gameInfo["Update"];
-            conf.Dirs.DLC = (string)gameInfo["DLC"] == "NOT FOUND" ? "" : (string)gameInfo["DLC"];
+                if (conf.Install.Base)
+                    conf.Install.Base = !(bool)gameInfo["Game_IsInstalled"];
 
-            conf.Install.Base = !(bool)gameInfo["Game_IsInstalled"];
-            conf.Install.Update = !(bool)gameInfo["Update_IsInstalled"];
-            conf.Install.DLC = !(bool)gameInfo["DLC_IsInstalled"];
-            conf.Install.Cemu = !File.Exists($"{gameInfo["Cemu"]}\\Cemu.exe");
-            conf.Install.Python = !File.Exists($"{gameInfo["Python"]}\\python38.dll") || !File.Exists($"{gameInfo["Python"]}\\python37.dll");
+                if (conf.Install.Cemu)
+                    conf.Install.Cemu = !File.Exists($"{gameInfo["Cemu"]}\\Cemu.exe");
 
-            /// Get system information and update config -/-
+                conf.Install.Update = !(bool)gameInfo["Update_IsInstalled"];
+                conf.Install.DLC = !(bool)gameInfo["DLC_IsInstalled"];
+                conf.Install.Python = !(File.Exists($"{gameInfo["Python"]}\\python38.dll") || File.Exists($"{gameInfo["Python"]}\\python37.dll"));
 
-            /// END BLOCK ONE
+                // Override
+                conf.Install.Base = false;
+                conf.Install.Update = true;
+                conf.Install.DLC = true;
+                conf.Dirs.Dynamic = "D:\\Cemu_Test";
+                conf.Install.Cemu = true;
+            }
 
-            /// Install -\-
+            // Update shortcut parameters
+            Shortcuts.Conf = conf;
 
+            // Create install tasks
             List<Task> t1 = new();
             List<Task> t2 = new();
 
             // Install Python
-            if (conf.Install.Python) t1.Add(RuntimeInstallers.Python(Interface.WriteLine, conf.Dirs.Python, conf.Install.PythonVersion, conf.Install.PythonDocs));
+            if (conf.Install.Python)
+                t1.Add(RuntimeInstallers.Python(print, conf.Dirs.Python));
 
-            /// Install -/-
+            // Install Cemu
+            if (conf.Install.Cemu)
+            {
+                // Define mlc01
+                string mlc01 = conf.Dirs.MLC01 == "" ? $"{conf.Dirs.Dynamic}\\mlc01" : conf.Dirs.MLC01;
+                Directory.CreateDirectory(mlc01);
 
+                // Install Game
+                if (conf.Install.Base)
+                    t2.Add(
+                        Batch.CopyDirectoryWithUpdate(update, print, 30, conf.Dirs.Base.FileCount(), "./install_log.txt", conf.Dirs.Base,
+                            $"{mlc01}\\usr\\title\\00050000\\{GameInfo.GetTitleID(conf.Dirs.Base, TitleIDFormat.HexEnd)}"
+                        )
+                    );
+
+                // Install Update
+                if (conf.Install.Update)
+                    t2.Add(
+                        Batch.CopyDirectoryWithUpdate(
+                            update, print, 30, conf.Dirs.Update.FileCount(), "./install_log.txt", conf.Dirs.Update,
+                            $"{mlc01}\\usr\\title\\0005000e\\{GameInfo.GetTitleID(conf.Dirs.Update, TitleIDFormat.HexEnd)}"
+                        )
+                    );
+
+                // Install DLC
+                if (conf.Install.DLC && conf.Dirs.DLC != "")
+                    t2.Add(
+                        Batch.CopyDirectoryWithUpdate(
+                            update, print, 30, conf.Dirs.DLC.FileCount(), "./install_log.txt", conf.Dirs.DLC,
+                            $"{mlc01}\\usr\\title\\0005000c\\{GameInfo.GetTitleID(conf.Dirs.DLC, TitleIDFormat.HexEnd)}"
+                        )
+                    );
+
+                // Install Cemu
+                t2.Add(Tasks.Cemu(update, print, conf));
+            }
+
+            await Task.WhenAll(t1);
+
+            t2.Add(Tasks.BCML(update, print, conf));
+            t2.Add(Tasks.Mods(update, print, conf));
+
+            await Task.WhenAll(t2);
+
+            // Create Cemu shortcut(s)
+            print($"[INSTALL] Creating Cemu.lnk . . .");
+            await Shortcuts.Write(conf.Shortcuts.Cemu);
+
+            // Create Botw shortcut(s)
+            print($"[INSTALL] Creating BOTW.lnk . . .");
+            await Shortcuts.Write(conf.Shortcuts.BotW);
+
+            // Create BCML shortcut(s)
+            print($"[INSTALL] Creating BCML.lnk . . .");
+            await Shortcuts.Write(conf.Shortcuts.BCML);
+
+            // Create DS4Windows shortcut(s)
+            if (conf.ControllerApi == "DSUController")
+            {
+                print($"[INSTALL] Creating DS4Windows.lnk . . .");
+                await Shortcuts.Write(conf.Shortcuts.DS4Windows);
+            }
+
+            return conf;
         }
     }
 }
--- BotwInstaller.Lib/Tasks.cs
+++ BotwInstaller.Lib/Tasks.cs
@@ -1,4 +1,7 @@
-﻿using static BotwInstaller.Lib.Config;
+﻿#pragma warning disable CS8602
+#pragma warning disable CS8604
+
+using static BotwInstaller.Lib.Config;
 
 using BotwScripts.Lib.Common;
 using BotwScripts.Lib.Common.Computer.Software;
@@ -9,6 +12,9 @@ using System.IO.Compression;
 using BotwInstaller.Lib.Configurations.Cemu;
 using BotwInstaller.Lib.Configurations.Shortcuts;
 using BotwScripts.Lib.Common.IO.FileSystems;
+using BotwScripts.Lib.Common.Computer;
+using BotwInstaller.Lib.Configurations;
+using BotwScripts.Lib.Common.IO;
 
 namespace BotwInstaller.Lib
 {
@@ -19,16 +25,16 @@ namespace BotwInstaller.Lib
         /// </summary>
         /// <param name="option"></param>
         /// <param name="update"></param>
-        /// <param name="notify"></param>
+        /// <param name="print"></param>
         /// <param name="installDir"></param>
         /// <param name="installGfx"></param>
         /// <param name="installRuntimes"></param>
         /// <returns></returns>
-        public static async Task Cemu(Interface.Update update, Interface.Notify notify, Config conf)
+        public static async Task Cemu(Interface.Update update, Interface.Notify print, Config conf)
         {
-            List<Task> download = new List<Task>();
-            List<Task> unpack = new List<Task>();
-            List<Task> install = new List<Task>();
+            List<Task> download = new();
+            List<Task> unpack = new();
+            List<Task> install = new();
 
             Directory.CreateDirectory($"{AppData}\\Temp\\BOTW");
             Directory.CreateDirectory($"{conf.Dirs.Dynamic}\\graphicPacks");
@@ -36,43 +42,66 @@ namespace BotwInstaller.Lib
             update(5, "cemu");
 
             // Download Cemu
-            notify($"{func} Downloading Cemu . . .");
+            print($"{func} Downloading Cemu . . .");
             download.Add(Download.FromUrl(DownloadLinks.Cemu, $"{AppData}\\Temp\\BOTW\\CEMU.PACK.res"));
 
             // Download GFX
-            notify($"{func} Downloading GFX . . .");
+            print($"{func} Downloading GFX . . .");
             download.Add(Download.FromUrl(await GitHub.GetLatestRelease("ActualMandM;cemu_graphic_packs"), $"{AppData}\\Temp\\BOTW\\GFX.PACK.res"));
 
+            // Download DS4
+            if (conf.ControllerApi == "DSUController")
+            {
+                print($"{func} Downloading DS4Windows . . .");
+                download.Add(Download.FromUrl(await GitHub.GetLatestRelease("Ryochan7;DS4Windows", 2), $"{AppData}\\Temp\\BOTW\\DS4.PACK.res"));
+            }
+
             // Download CemuHook
-            notify($"{func} Downloading CemuHook . . .");
+            print($"{func} Downloading CemuHook . . .");
             download.Add(Download.FromUrl("https://files.sshnuke.net/cemuhook_1262d_0577.zip", $"{AppData}\\Temp\\BOTW\\CEMUHOOK.PACK.res"));
 
             // Install runtimes
-            notify($"{func} Installing runtimes . . .");
-            install.Add(RuntimeInstallers.VisualCRuntime(notify));
+            print($"{func} Installing runtimes . . .");
+            install.Add(RuntimeInstallers.VisualCRuntime(print));
 
             // Wait for download
             await Task.WhenAll(download); update(45, "cemu");
 
             // Unpack cemu to the temp folder
-            notify($"{func} Extracting Cemu . . .");
+            print($"{func} Extracting Cemu . . .");
             unpack.Add(Task.Run(() => ZipFile.ExtractToDirectory($"{AppData}\\Temp\\BOTW\\CEMU.PACK.res", $"{AppData}\\Temp\\BOTW\\CEMU")));
 
             // Unpack GFX to graphicPacks folder
-            notify($"{func} Extracting GFX . . .");
+            print($"{func} Extracting GFX . . .");
             unpack.Add(Task.Run(() => ZipFile.ExtractToDirectory($"{AppData}\\Temp\\BOTW\\GFX.PACK.res", $"{conf.Dirs.Dynamic}\\graphicPacks\\downloadedGraphicPacks")));
 
             // Unpack CemuHook to the temp folder
-            notify($"{func} Extracting CemuHook . . .");
+            print($"{func} Extracting CemuHook . . .");
             unpack.Add(Task.Run(() => ZipFile.ExtractToDirectory($"{AppData}\\Temp\\BOTW\\CEMUHOOK.PACK.res", $"{AppData}\\Temp\\BOTW\\CEMUHOOK")));
+
+            // Download DS4
+            if (conf.ControllerApi == "DSUController")
+            {
+                print($"{func} Extracting DS4Windows . . .");
+                unpack.Add(Task.Run(() => ZipFile.ExtractToDirectory($"{AppData}\\Temp\\BOTW\\DS4.PACK.res", AppData, true)));
+            }
 
             // Wait for unpack
             await Task.WhenAll(unpack); update(70, "cemu");
 
-            // Add GFX folder
-            notify($"{func} Installing Cemu . . .");
+            // Install Cemu
+            print($"{func} Installing Cemu . . .");
             install.Add(Task.Run(() => {
+                // Setup fonders and variables
+                Directory.CreateDirectory($"{conf.Dirs.Dynamic}\\sharedFonts");
                 string cemuTemp = $"{AppData}\\Temp\\BOTW\\CEMU".SubFolder();
+                string[] sharedFonts = new string[] { "CafeCn.ttf", "CafeTw.ttf", "CafeStd.ttf", "CafeKr.ttf" };
+
+                // Copy sharedFonts
+                foreach (string font in sharedFonts)
+                    File.Copy($"{cemuTemp}\\resources\\sharedFonts\\{font}", $"{conf.Dirs.Dynamic}\\sharedFonts\\{font}");
+
+                // Copy Cemu files
                 foreach (var file in Directory.EnumerateFiles(cemuTemp, "*.*", SearchOption.AllDirectories))
                 {
                     var dir = new FileInfo(file).DirectoryName;
@@ -81,20 +110,16 @@ namespace BotwInstaller.Lib
                 }
             }));
 
-            // Group lnk creation
-            install.Add(Task.Run(async () =>
-            {
-                // Create Cemu shortcuts
-                notify($"{func} Creating Cemu.lnk . . .");
-                await Shortcuts.Write(conf.Shortcuts.Cemu);
-
-                // Configure Botw shortcuts
-                notify($"{func} Creating BOTW.lnk . . .");
-                await Shortcuts.Write(conf.Shortcuts.BotW);
+            // Install CemuHook
+            print($"{func} Installing Cemu . . .");
+            install.Add(Task.Run(() => {
+                string cemuHookTemp = $"{AppData}\\Temp\\BOTW\\CEMUHOOK";
+                foreach (var file in Directory.EnumerateFiles(cemuHookTemp, "*.*", SearchOption.AllDirectories))
+                    File.Copy(file, $"{conf.Dirs.Dynamic}\\{file.Replace(cemuHookTemp, "")}");
             }));
 
             // Configure settings, profiles, and controllers
-            notify($"{func} Configuring Cemu . . .");
+            print($"{func} Configuring Cemu . . .");
             install.Add(Task.Run(() =>
             {
                 // Create new controller profile
@@ -115,6 +140,91 @@ namespace BotwInstaller.Lib
             download.Clear();
             unpack.Clear();
             install.Clear();
+        }
+
+        public static async Task BCML(Interface.Update update, Interface.Notify print, Config conf)
+        {
+            string func = "[INSTALL.BCML]";
+
+            update(5, "bcml");
+            if (!File.Exists($"{conf.Dirs.Python}\\Scripts\\pip.exe"))
+            {
+                print($"{func} Installing PIP");
+                await HiddenProcess.Start($"{conf.Dirs.Python}\\Scripts\\python.exe", "-m pip install --upgrade pip");
+            }
+
+            List<Task> pip = new();
+
+            print($"{func} Installing BCML");
+            update(8, "bcml");
+            pip.Add(HiddenProcess.Start($"{conf.Dirs.Python}\\Scripts\\pip.exe", "install --force-reinstall bcml"));
+
+            print($"{func} Installing CEF");
+            update(14, "bcml");
+            pip.Add(HiddenProcess.Start($"{conf.Dirs.Python}\\Scripts\\pip.exe", "install --force-reinstall cefpython3"));
+
+            print($"{func} Installing TK");
+            pip.Add(HiddenProcess.Start($"{conf.Dirs.Python}\\Scripts\\pip.exe", "install --force-reinstall tk"));
+
+            print($"{func} Creating Settings");
+            update(20, "bcml");
+            pip.Add(BcmlSettings.Write(conf));
+
+            await Task.WhenAll(pip);
+            update(35, "bcml");
+        }
+
+        public static async Task Mods(Interface.Update update, Interface.Notify print, Config conf)
+        {
+            List<Task> installTasks = new();
+            List<string?> mods = conf.ModPacks["wiiu"][conf.ModPack];
+            string func = "[INSTALL.BCML.MODS]";
+
+            if (conf.IsNX) mods = conf.ModPacks["switch"][conf.ModPack];
+
+            await Download.FromUrl("https://raw.githubusercontent.com/ArchLeaders/Botw-Installer/master/RE/python_module.py", $"{AppData}\\Temp\\BOTW\\python.sh");
+            update(50, "bcml");
+
+            for (int i = 0; i < mods.Count; i++)
+            {
+                if (mods[i] != null)
+                {
+                    installTasks.Add(Task.Run(async() =>
+                    {
+                        string last = "false";
+
+                        if (mods.Count == i + 1)
+                            last = "true";
+
+                        print($"{func} Installing {mods[i]} . . .");
+                        await Download.FromUrl(mods[i], $"{AppData}\\Temp\\BOTW\\MOD__{i}.bnp");
+                        await HiddenProcess.Start($"{conf.Dirs.Python}\\python.exe", $"\"{AppData}\\Temp\\BOTW\\python.sh\" \"{AppData}\\Temp\\BOTW\\MOD__{i}.bnp\" {last}");
+                    }));
+                }
+            }
+
+            await Task.WhenAll(installTasks);
+            update(80, "bcml");
+
+            if (Directory.Exists($"{conf.Dirs.BCML}\\mods\\9999_BCML"))
+            {
+                print($"{func} Merging mods . . .");
+                if (conf.UseCemu)
+                {
+                    await Task.Run(() => Batch.CopyDirectory($"{conf.Dirs.BCML}\\mods\\9999_BCML", $"{conf.Dirs.Dynamic}\\graphicPacks\\BreathOfTheWild_BCML"));
+                }
+                else
+                {
+                    await Task.Run(() => Batch.CopyDirectory($"{conf.Dirs.BCML}\\mods\\9999_BCML", conf.Dirs.Dynamic));
+                }
+            }
+
+            update(100, "bcml");
+        }
+
+        public static void Homebrew(Interface.Notify print, Config conf)
+        {
+
         }
     }
 }
