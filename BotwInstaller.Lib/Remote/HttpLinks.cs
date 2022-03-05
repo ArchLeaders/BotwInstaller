7d7822386af93ca4e86e18b5d70a0c6a1ddc7ca8
ArchLeaders <archparliament@gmail.com>
2022-03-05 14:55:16 -0800

    Update source links

--- BotwInstaller.Lib/Remote/HttpLinks.cs
+++ BotwInstaller.Lib/Remote/HttpLinks.cs
@@ -6,9 +6,9 @@
         public static string BcmlIconFile { get; } = "https://github.com/ArchLeaders/Botw-Installer/raw/master/Resources/Bcml.ico.res";
         public static string BotwIconFile { get; } = "https://github.com/ArchLeaders/Botw-Installer/raw/master/Resources/Botw.ico.res";
         public static string BotwBatchFile { get; } = "https://raw.githubusercontent.com/ArchLeaders/Botw-Installer/master/RE/BatchFiles/botw.bat";
-        public static string BcmlBatchFile { get; } = "";
+        public static string BcmlBatchFile { get; } = "https://raw.githubusercontent.com/ArchLeaders/Botw-Installer/master/RE/BatchFiles/bcml.bat";
         public static string CemuBatchFile { get; } = "https://raw.githubusercontent.com/ArchLeaders/Botw-Installer/master/RE/BatchFiles/RemoveCemu.bat";
-        public static string DS4BatchFile { get; } = "";
+        public static string DS4BatchFile { get; } = "https://raw.githubusercontent.com/ArchLeaders/Botw-Installer/master/RE/BatchFiles/ds4windows.bat";
 
     }
 }
