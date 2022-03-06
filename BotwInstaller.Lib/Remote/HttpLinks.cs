a8a7ef6002fc2f8bd1ddf6dadabe3be38678051f
ArchLeaders <archparliament@gmail.com>
2022-03-06 11:18:32 -0800

    Add Pip Installer

--- BotwInstaller.Lib/Remote/HttpLinks.cs
+++ BotwInstaller.Lib/Remote/HttpLinks.cs
@@ -7,8 +7,9 @@
         public static string BotwIconFile { get; } = "https://github.com/ArchLeaders/Botw-Installer/raw/master/Resources/Botw.ico.res";
         public static string BotwBatchFile { get; } = "https://raw.githubusercontent.com/ArchLeaders/Botw-Installer/master/RE/BatchFiles/botw.bat";
         public static string BcmlBatchFile { get; } = "https://raw.githubusercontent.com/ArchLeaders/Botw-Installer/master/RE/BatchFiles/bcml.bat";
-        public static string CemuBatchFile { get; } = "https://raw.githubusercontent.com/ArchLeaders/Botw-Installer/master/RE/BatchFiles/RemoveCemu.bat";
+        public static string CemuBatchFile { get; } = "https://raw.githubusercontent.com/ArchLeaders/Botw-Installer/master/RE/BatchFiles/cemu.bat";
         public static string DS4BatchFile { get; } = "https://raw.githubusercontent.com/ArchLeaders/Botw-Installer/master/RE/BatchFiles/ds4windows.bat";
+        public static string PipInstaller { get; } = "https://bootstrap.pypa.io/get-pip.py";
 
     }
 }
