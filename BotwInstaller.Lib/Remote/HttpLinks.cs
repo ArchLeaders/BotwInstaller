ad4ad2e0030a62850ac3fb5b3fec5cce4f8ce53c
ArchLeaders <archparliament@gmail.com>
2022-03-06 17:24:38 -0800

    Update remote links

--- BotwInstaller.Lib/Remote/HttpLinks.cs
+++ BotwInstaller.Lib/Remote/HttpLinks.cs
@@ -1,15 +1,17 @@
-﻿namespace BotwInstaller.Lib.Remote
+﻿using BotwScripts.Lib.Common.Web.GitHub;
+
+namespace BotwInstaller.Lib.Remote
 {
     public class HttpLinks
     {
-        public static string ModPresetsJson { get; } = "https://raw.githubusercontent.com/ArchLeaders/Botw-Installer/master/RE/TempDataFromRE.json";
-        public static string BcmlIconFile { get; } = "https://github.com/ArchLeaders/Botw-Installer/raw/master/Resources/Bcml.ico.res";
-        public static string BotwIconFile { get; } = "https://github.com/ArchLeaders/Botw-Installer/raw/master/Resources/Botw.ico.res";
-        public static string BotwBatchFile { get; } = "https://raw.githubusercontent.com/ArchLeaders/Botw-Installer/master/RE/BatchFiles/botw.bat";
-        public static string BcmlBatchFile { get; } = "https://raw.githubusercontent.com/ArchLeaders/Botw-Installer/master/RE/BatchFiles/bcml.bat";
-        public static string CemuBatchFile { get; } = "https://raw.githubusercontent.com/ArchLeaders/Botw-Installer/master/RE/BatchFiles/cemu.bat";
-        public static string DS4BatchFile { get; } = "https://raw.githubusercontent.com/ArchLeaders/Botw-Installer/master/RE/BatchFiles/ds4windows.bat";
+        public static string ModPresets { get; } = "https://raw.githubusercontent.com/ArchLeaders/Botw-Installer/master/RE/TempDataFromRE.json";
+        public static string BcmlIconFile { get; } = "https://github.com/ArchLeaders/BotwInstaller/raw/master/BotwInstaller.Lib/Configurations/Icons/bcml.ico";
+        public static string BotwIconFile { get; } = "https://github.com/ArchLeaders/BotwInstaller/raw/master/BotwInstaller.Lib/Configurations/Icons/botw.ico";
+        public static string BotwBatchFile { get; } = "https://raw.githubusercontent.com/ArchLeaders/BotwInstaller/master/BotwInstaller.Lib/Configurations/BatchFiles/botw.bat";
+        public static string BcmlBatchFile { get; } = "https://raw.githubusercontent.com/ArchLeaders/BotwInstaller/master/BotwInstaller.Lib/Configurations/BatchFiles/bcml.bat";
+        public static string CemuBatchFile { get; } = "https://raw.githubusercontent.com/ArchLeaders/BotwInstaller/master/BotwInstaller.Lib/Configurations/BatchFiles/cemu.bat";
+        public static string DS4BatchFile { get; } = "https://raw.githubusercontent.com/ArchLeaders/BotwInstaller/master/BotwInstaller.Lib/Configurations/BatchFiles/ds4windows.bat";
         public static string PipInstaller { get; } = "https://bootstrap.pypa.io/get-pip.py";
-
+        public static string ModInstaller { get; } = "https://raw.githubusercontent.com/ArchLeaders/BotwInstaller/master/BotwInstaller.Lib/Configurations/Python/python_module.py";
     }
 }
