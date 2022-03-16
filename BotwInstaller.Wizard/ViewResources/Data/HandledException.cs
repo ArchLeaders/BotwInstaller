5dbc19b6d833050ca31c6ad10cef73c1bb7a6062
ArchLeaders <archparliament@gmail.com>
2022-03-15 19:47:43 -0700

    HandledException class

---
+++ BotwInstaller.Wizard/ViewResources/Data/HandledException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace BotwInstaller.Wizard.ViewResources.Data
+{
+    public class HandledException
+    {
+        public string Exception { get; set; } = "Exception Thrown";
+        public string Message { get; set; } = "";
+        public string? ExtendedMessage { get; set; } = null;
+    }
+}
