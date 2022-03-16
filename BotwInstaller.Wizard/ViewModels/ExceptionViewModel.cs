e8f1dd6b4e4f511a2264cc91b6dd7d40d8f22ebf
ArchLeaders <archparliament@gmail.com>
2022-03-15 19:32:28 -0700

    Exception page in it's own View/ViewModel

---
+++ BotwInstaller.Wizard/ViewModels/ExceptionViewModel.cs
@@ -0,0 +1,41 @@
+﻿using Stylet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BotwInstaller.Wizard.ViewModels
+{
+    public class ExceptionViewModel : Screen
+    {
+
+        private string _title = "Exception Thrown";
+        public string Title
+        {
+            get => _title;
+            set => SetAndNotify(ref _title, value);
+        }
+
+        private string _message = "Invalid Exception";
+        public string Message
+        {
+            get => _message;
+            set => SetAndNotify(ref _message, value);
+        }
+
+        private string _extendedMessage = "No details were provided.";
+        public string ExtendedMessage
+        {
+            get => _extendedMessage;
+            set => SetAndNotify(ref _extendedMessage, value);
+        }
+
+        private bool _isReportable = true;
+        public bool IsReportable
+        {
+            get => _isReportable;
+            set => SetAndNotify(ref _isReportable, value);
+        }
+    }
+}
