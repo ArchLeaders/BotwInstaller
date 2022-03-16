310869561c990e691f16ac6a879721e5128972e2
ArchLeaders <archparliament@gmail.com>
2022-03-15 19:46:28 -0700

    Splash page use it's own View/ViewModel

---
+++ BotwInstaller.Wizard/ViewModels/SplashViewModel.cs
@@ -0,0 +1,33 @@
+﻿using Stylet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace BotwInstaller.Wizard.ViewModels
+{
+    public class SplashViewModel : Screen
+    {
+        public void Open(string gameMode)
+        {
+            Mode = gameMode;
+            Shell.SetupPageVisibility = Visibility.Visible;
+        }
+        
+        private string _mode = "wiiu";
+        public string Mode
+        {
+            get => _mode;
+            set => SetAndNotify(ref _mode, value);
+        }
+
+        public ShellViewModel Shell;
+
+        public SplashViewModel(ShellViewModel shell)
+        {
+            Shell = shell;
+        }
+    }
+}
