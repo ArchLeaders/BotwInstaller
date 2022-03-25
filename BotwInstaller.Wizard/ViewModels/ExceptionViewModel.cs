118bbe6b2d9ac2a5996a7d048f04d182f3cd133a
ArchLeaders <archparliament@gmail.com>
2022-03-25 03:23:37 -0700

    Report and Copy actions

--- BotwInstaller.Wizard/ViewModels/ExceptionViewModel.cs
+++ BotwInstaller.Wizard/ViewModels/ExceptionViewModel.cs
@@ -4,11 +4,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BotwInstaller.Wizard.Helpers;
 
 namespace BotwInstaller.Wizard.ViewModels
 {
     public class ExceptionViewModel : Screen
     {
+        public async Task Report()
+        {
+            if (ShellViewModel != null)
+                await GitIssue.ReportAsMarkdown(ShellViewModel);
+        }
+
+        public async Task Copy()
+        {
+            if (ShellViewModel != null)
+                await GitIssue.ViewReportAsHtml(ShellViewModel);
+        }
 
         private string _title = "Exception Thrown";
         public string Title
@@ -31,11 +43,20 @@ namespace BotwInstaller.Wizard.ViewModels
             set => SetAndNotify(ref _extendedMessage, value);
         }
 
+        private string _contactInfo = "Anonymous";
+        public string ContactInfo
+        {
+            get => _contactInfo;
+            set => SetAndNotify(ref _contactInfo, value);
+        }
+
         private bool _isReportable = true;
         public bool IsReportable
         {
             get => _isReportable;
             set => SetAndNotify(ref _isReportable, value);
         }
+
+        public ShellViewModel? ShellViewModel = null;
     }
 }
