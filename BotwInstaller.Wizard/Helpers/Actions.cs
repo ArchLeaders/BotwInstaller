f1195e203bd7d0d8994d208b9b35fd5beed6347c
ArchLeaders <archparliament@gmail.com>
2022-05-02 16:35:52 -0700

    Add hyperlink actions

---
+++ BotwInstaller.Wizard/Helpers/Actions.cs
@@ -0,0 +1,13 @@
+﻿#pragma warning disable IDE0060
+
+using System.Operations;
+using System.Windows.Documents;
+using System.Windows.Navigation;
+
+namespace BotwInstaller.Wizard.Helpers
+{
+    class Actions
+    {
+        public static void OpenURL(Hyperlink sender, RequestNavigateEventArgs e) => Execute.Url(sender.NavigateUri);
+    }
+}
