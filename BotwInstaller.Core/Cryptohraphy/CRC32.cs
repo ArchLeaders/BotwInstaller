1d5377ffe9aeec3a15d4c269a5f60824886498a2
ArchLeaders <archparliament@gmail.com>
2022-08-22 18:51:18 -0700

    Add the CRC32 hash algorithm

---
+++ BotwInstaller.Core/Cryptohraphy/CRC32.cs
@@ -0,0 +1,113 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace BotwInstaller.Core.Cryptohraphy
+{
+    public sealed class CRC32 : HashAlgorithm
+    {
+        public override int HashSize => 32;
+
+        public const uint DefaultPolynomial = 0xedb88320u;
+        public const uint DefaultSeed = 0xffffffffu;
+
+        static uint[]? defaultTable;
+
+        readonly uint seed;
+        readonly uint[] table;
+        uint hash;
+
+        public CRC32() : this(DefaultPolynomial, DefaultSeed) { }
+        public CRC32(uint polynomial, uint seed)
+        {
+            if (!BitConverter.IsLittleEndian)
+                throw new PlatformNotSupportedException("Not supported on Big Endian processors");
+
+            table = InitializeTable(polynomial);
+            this.seed = hash = seed;
+        }
+
+        public override void Initialize()
+        {
+            hash = seed;
+        }
+
+        protected override void HashCore(byte[] array, int ibStart, int cbSize)
+        {
+            hash = CalculateHash(table, hash, array, ibStart, cbSize);
+        }
+
+        protected override byte[] HashFinal()
+        {
+            var hashBuffer = UInt32ToBigEndianBytes(~hash);
+            HashValue = hashBuffer;
+            return hashBuffer;
+        }
+
+
+        public static uint Compute(byte[] buffer)
+        {
+            return Compute(DefaultSeed, buffer);
+        }
+
+        public static uint Compute(uint seed, byte[] buffer)
+        {
+            return Compute(DefaultPolynomial, seed, buffer);
+        }
+
+        public static uint Compute(uint polynomial, uint seed, byte[] buffer)
+        {
+            return ~CalculateHash(InitializeTable(polynomial), seed, buffer, 0, buffer.Length);
+        }
+
+        static uint[] InitializeTable(uint polynomial)
+        {
+            if (polynomial == DefaultPolynomial && defaultTable != null)
+                return defaultTable;
+
+            var createTable = new uint[256];
+            for (var i = 0; i < 256; i++) {
+                var entry = (uint)i;
+                for (var j = 0; j < 8; j++)
+                    if ((entry & 1) == 1)
+                        entry = (entry >> 1) ^ polynomial;
+                    else
+                        entry >>= 1;
+                createTable[i] = entry;
+            }
+
+            if (polynomial == DefaultPolynomial)
+                defaultTable = createTable;
+
+            return createTable;
+        }
+
+        static uint CalculateHash(uint[] table, uint seed, IList<byte> buffer, int start, int size)
+        {
+            var hash = seed;
+            for (var i = start; i < start + size; i++)
+                hash = (hash >> 8) ^ table[buffer[i] ^ hash & 0xff];
+            return hash;
+        }
+
+        static byte[] UInt32ToBigEndianBytes(uint uint32)
+        {
+            var result = BitConverter.GetBytes(uint32);
+
+            if (BitConverter.IsLittleEndian)
+                Array.Reverse(result);
+
+            return result;
+        }
+
+        public static string ComputeHash(string str)
+        {
+            CRC32 crc32 = new();
+            string hash = string.Empty;
+            byte[] bytes = Encoding.ASCII.GetBytes(str);
+
+            foreach (byte b in crc32.ComputeHash(bytes)) hash += b.ToString("x2").ToLower();
+
+            return Convert.ToUInt32(hash, 16).ToString();
+        }
+    }
+}
