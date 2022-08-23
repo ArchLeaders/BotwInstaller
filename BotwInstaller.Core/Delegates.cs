d727f77c36852421053037f0938a9654e4ed8657
ArchLeaders <archparliament@gmail.com>
2022-08-22 18:49:30 -0700

    Create generic delegates

---
+++ BotwInstaller.Core/Delegates.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BotwInstaller.Core
+{
+    public delegate void Print(string str);
+
+    public delegate void Update(decimal value);
+}
