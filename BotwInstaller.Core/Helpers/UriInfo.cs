5cc596518b0463867cfc5986ebadccaf456de0ad
ArchLeaders <archparliament@gmail.com>
2022-08-22 18:55:29 -0700

    UriInfo helper class; Add DotNET uri definitions

---
+++ BotwInstaller.Core/Helpers/UriInfo.cs
@@ -0,0 +1,20 @@
+﻿namespace BotwInstaller.Core.Helpers
+{
+    public class UriInfo
+    {
+        private static Dictionary<string, object> cache = new();
+
+        public static string Get(string key)
+        {
+            Sync();
+            return (string)cache[key];
+        }
+
+        public static void Sync(bool reload = false)
+        {
+            if (cache.Count == 0 || reload) {
+                cache = new Resource("BotwInstaller.Core/Data/UriInfo.json").ToJson<Dictionary<string, object>>() ?? new();
+            }
+        }
+    }
+}
