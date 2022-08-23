90d392c84e8cf805e73ada2d3530f826ade9abec
ArchLeaders <archparliament@gmail.com>
2022-08-22 18:52:08 -0700

    Create iteration (string) extension in `Common`

---
+++ BotwInstaller.Core/Extensions/Common.cs
@@ -0,0 +1,18 @@
+﻿using BotwInstaller.Core.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BotwInstaller.Core.Extensions
+{
+    public static class Common
+    {
+        public static async Task<List<string>> GetFilesAsync(this string path, string filter)
+            => await new IterationBuilder(path, filter).GetAsync();
+
+        public static async Task<List<string>> GetFilesAsync(this DirectoryInfo path, string filter)
+            => await new IterationBuilder(path.FullName, filter).GetAsync();
+    }
+}
