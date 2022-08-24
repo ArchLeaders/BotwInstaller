3a672e6c2544c53edf8b97add4c7dee94da0b144
ArchLeaders <archparliament@gmail.com>
2022-08-24 10:30:57 -0700

    Support remote resources and Yaz0 compression

--- BotwInstaller.Core/Helpers/Resource.cs
+++ BotwInstaller.Core/Helpers/Resource.cs
@@ -2,6 +2,7 @@
 using Nintendo.Yaz0;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Reflection;
 using System.Text;
@@ -18,21 +19,28 @@ namespace BotwInstaller.Core.Helpers
         public Resource(string resource, bool isCompressed = false)
         {
             resource = resource.ToSystemPath();
+            byte[] data;
+
             Stream? stream = Assembly.GetCallingAssembly().GetManifestResourceStream($"BotwInstaller.{resource.Replace("\\", ".")}");
 
             if (stream == null) {
 
                 try {
+                    var url = $"{BaseUrl}/{resource.Replace("\\", "/")}?v={Random.Shared.Next(1, 100)}";
+
                     using HttpClient client = new();
-                    stream = client.GetStreamAsync($"{BaseUrl}/BotwInstaller.Core/{resource.Replace("\\", "/")}?v={Random.Shared.Next(1,100)}").Result;
+                    data = client.GetByteArrayAsync(url).Result;
                 }
-                catch { 
+                catch (Exception ex) {
+                    Debug.WriteLine(ex);
                     throw new FileNotFoundException($"Could not find the file 'BotwInstaller.{resource}'.");
                 }
             }
+            else {
+                using BinaryReader reader = new(stream);
+                data = reader.ReadBytes((int)stream.Length);
+            }
 
-            using BinaryReader reader = new(stream);
-            byte[] data = reader.ReadBytes((int)stream.Length);
             Data = isCompressed ? Yaz0.Decompress(data) : data;
         }
 
