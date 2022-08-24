2449a376a12bbda82771dc7f8c11537ce6c47b80
ArchLeaders <archparliament@gmail.com>
2022-08-24 10:32:51 -0700

    Update `Root` class

--- BotwInstaller.Core/Root.cs
+++ BotwInstaller.Core/Root.cs
@@ -1,13 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text;
 
 namespace BotwInstaller.Core
 {
     public class Root
     {
+        public string NetVersion { get; set; } = "Not Found";
+        public bool HasPython { get; set; } = false;
+        public bool HasPip { get; set; } = false;
+        public bool HasBcml { get; set; } = false;
+        public bool HasYuzu { get; set; } = false;
+        public bool HasCemu { get; set; } = false;
+        public bool HasSailedTheHighSeas { get; set; } = false;
+
+        public Dictionary<string, bool> BaseGame { get; set; } = new();
+        public Dictionary<string, bool> Update { get; set; } = new();
+        public Dictionary<string, bool> Dlc { get; set; } = new();
+        public Dictionary<string, bool> BaseGameNx { get; set; } = new();
+        public Dictionary<string, bool> DlcNx { get; set; } = new();
+        public Dictionary<string, bool> Cemu { get; set; } = new();
+        public Dictionary<string, bool> Python { get; set; } = new();
+        public List<string> Yuzu { get; set; } = new();
+
+        public Dictionary<string, List<string>> BaseGameMissing { get; set; } = new();
+        public Dictionary<string, List<string>> UpdateMissing { get; set; } = new();
+        public Dictionary<string, List<string>> DlcMissing { get; set; } = new();
+        public Dictionary<string, List<string>> BaseGameNxMissing { get; set; } = new();
+        public Dictionary<string, List<string>> DlcNxMissing { get; set; } = new();
+
+        public List<string> UKingFiles { get; set; } = new();
+        public List<string> RollPictFiles { get; set; } = new();
+        public List<string> AglResourceFiles { get; set; } = new();
+        public List<string> CemuFiles { get; set; } = new();
+        public List<string> PythonFiles { get; set; } = new();
+        public List<string> PythonPathDirs { get; set; } = new();
+        public List<string> TheHighSeas { get; set; } = new();
+
+
         public object? this[string key]
         {
             get {
@@ -28,25 +56,25 @@ namespace BotwInstaller.Core
             }
         }
 
-        // Assigned Properties
-        public string BaseGame { get; set; } = "Not Found";
-        public string Update { get; set; } = "Not Found";
-        public string Dlc { get; set; } = "Not Found";
-        public string BaseGameNx { get; set; } = "Not Found";
-        public string DlcNX { get; set; } = "Not Found";
-        public string Cemu { get; set; } = "Not Found";
-        public string Yuzu { get; set; } = "Not Found";
-        public string Python { get; set; } = "Not Found";
-        public string PythonVersion { get; set; } = "Not Found";
-        public string NetVersion { get; set; } = "Not Found";
-        public bool HasPython { get; set; } = false;
-        public bool HasBcml { get; set; } = false;
-        public bool HasYuzu { get; set; } = false;
-        public bool HasCemu { get; set; } = false;
+        public object? GetDefault(string name)
+        {
+            var obj = this[name];
+            if (obj is List<string> list) {
+                return list.FirstOrDefault("Not Found");
+            }
+            else {
+                return obj;
+            }
+        }
 
-        // Floating Data
-        public List<string> UKingFiles { get; set; } = new();
-        public List<string> RollPictFiles { get; set; } = new();
-        public List<string> PythonFiles { get; set; } = new();
+        public override string ToString()
+        {
+            StringBuilder str = new();
+            foreach (var prop in typeof(Root).GetProperties()) {
+                str.AppendLine($"{prop.Name}: {GetDefault(prop.Name)}");
+            }
+
+            return str.ToString();
+        }
     }
 }
